1462bc18931d8988b42e44e25bc203d0c67624cd
giro <e_x_tra@naver.com>
2021-08-03 01:45:12 +0900

    feat : Posts 수정/조회 기능 추가

--- src/main/java/com/giro/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/giro/book/springboot/service/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.giro.book.springboot.service.posts;
 
+import com.giro.book.springboot.domain.posts.Posts;
 import com.giro.book.springboot.domain.posts.PostsRepository;
+import com.giro.book.springboot.web.dto.PostsResponseDto;
 import com.giro.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다.")
+        );
+        return new PostsResponseDto(entity);
+    }
+
+    public Long update(Long id, PostsSaveRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                ()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
+        );
+
+        // 이 부분에서 db에 쿼리를 날리는 부분이 x -> JPA의 영속성 컨텍스트이기때문에 가능
+        // 따라서 해당 데이터 값을 변경하면 "트랙젠션이 끝나는 시점"에 해당 테이블에 변경분을 반영
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
     }
 }
